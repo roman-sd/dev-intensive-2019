e995930f408d2fa0e3891d9b74bea439a38a5f44
Sedykh Roman <sedykhroman@gmail.com>
2019-07-03 14:17:46 +0300

    DTO User, Factory

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,32 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.*
+
+data class User (
+    val id : String,
+    var firstName : String?,
+    var lastName : String?,
+    var avatar : String?,
+    var rating : Int = 0,
+    var respect : Int = 0,
+    var lastVisit : Date? = Date(),
+    var isOnline : Boolean = false
+) {
+    constructor(id: String, firstName: String?, lastName: String?) : this(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+
+    companion object Factory {
+        private var lastId : Int = -1
+        fun makeUser(fullName: String?) : User {
+            lastId++
+            val parts : List<String>? = fullName?.split(" ")
+            val firstName= parts?.getOrNull(0)
+            val lastName = parts?.getOrNull(1)
+            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+        }
+    }
+}
