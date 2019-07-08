942910d3216b6b483a2a86c4038750edc6ddc151
Sedykh Roman <sedykhroman@gmail.com>
2019-07-08 14:14:59 +0300

    String.stripHtml

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,9 +1,14 @@
 package ru.skillbranch.devintensive.extensions
 
-fun String.truncate(value: Int = 16) : String {
+fun String.truncate(value: Int = 16): String {
     return when {
         this.length >= value -> """${substring(0, value).trimEnd()}..."""
         else -> this
     }
+}
 
+fun String.stripHtml(): String {
+    return this
+        .replace("\\s+".toRegex(), " ")
+        .replace("<[^>]*>".toRegex(), "")
 }
